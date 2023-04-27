63513828b9ddff7c8550eaad4429b6163a45bc9b
keerthinanjundareddy <keerthana@apprikart.com>
2023-04-27 10:55:49 +0530

    made changes in content

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2023, 5, 20, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2023, 5, 5, 0)}); // year, month, date, hour
         });
 });		
 
